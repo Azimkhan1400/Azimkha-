ed0679ec039301ae993826dfaaf45b4e6233c43f
Azimkhan1400 <116316713+Azimkhan1400@users.noreply.github.com>
2022-10-21 03:38:47 +0430

    Azim1399.py
    apt update 
    
    apt upgrade 
    
    pkg install git 
    
    git clone https://github.com/Azimkhan1400/Azimkhan.git
    ls
    Cd Azim1399
    ls
    Python Azim1399.py

---
+++ Azim1399.py
@@ -0,0 +1,47 @@
+# afg
+Afg clone fast command  creted by mohammad sultani
+
+command
+pkg update && pkg upgrade 
+$ pkg install git 
+$ pkg install python2 
+$ pip2 install requests
+$ pip2 install mechanize 
+$ pip2 install bs4 
+$ git clone https://github.com/Mohammadja://t.me/AzimkhanzadaHhttps://www.facebook.com/profile.php?id=100080380340309ttps://t.me/sultani1122
+
+### Subscribe our channel on youtube:
+چنل یوتیوب را سبسکرایب کنید تشکر 
+
+https://fb.watch/ghq2PHVQEe/
+### Chekout our telegram chanel  :
+چنل تلگرام
+https://t.me/Azimkhanzada
+
+## 👥 *Join*
+
+### Gmail: ایمیل
+ 
+mohmmadazimrahimizada@gmail.com
+
+### Telegram id: آیدی خودم در تلگرام
+
+
+@Azimkhan1200
+
+### Facebook page:  جیمیل
+
+
+mohmmadazimrahimizada@gmail.com
+
+
+
+
+### My GitHub ID link:  سایت گیت هاب ما 
+
+
+https://github.com/Azimkhan1400/Azimkha-.git
+
+### 📢 Warning  هشدار ابزار های هک که گذاشتیم
+
+*This tool is only for educational purpose. If you use this tool for other purposes except education we will not be responsible in such cases.*
